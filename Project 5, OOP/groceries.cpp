a475877e6a40409490b972b6f124ece97158fef8
carsonkrueger <crkruege@gmail.com>
2021-04-16 11:29:33 -0600

    groceries.cpp updated
    -Added many things to main
    -Need to update Items Class to support amount variable to be able to be accesed when creating Payment Object for each Order Object.

--- Project 5, OOP/groceries.cpp
+++ Project 5, OOP/groceries.cpp
@@ -21,7 +21,7 @@ int find_item_idx(int);
 //GLOBAL VARIABLES
 vector<std::vector<std::string>> orders_vec = {};
 vector<std::vector<std::string>> items_vec = {};
-vector<Customer> customers_vec = {};
+vector<vector<std::string>> customers_vec = {};
 
 //=======================Order=======================
 class Order
@@ -57,17 +57,17 @@ private:
     std::string zip;
     std::string phone;
     std::string email;
+    std::vector<Order> cust_orders;
 
 public:
     Customer(int theId, std::string theName, std::string theStreet, std::string theCity,
-             std::string theState, std::string theZip, std::string thePhone, std::string theEmail)
+             std::string theState, std::string theZip, std::string thePhone, std::string theEmail, std::vector<Order> the_cust_orders)
         : cust_id(theId), name(theName), street(theStreet), city(theCity), state(theState),
-          zip(theZip), phone(thePhone), email(theEmail){};
+          zip(theZip), phone(thePhone), email(theEmail), cust_orders(the_cust_orders){};
     // ~Customer()
     // {
     //     int id =
     // }
-    std::vector<Order> cust_orders;
     int get_cust_id() const
     {
         return this->cust_id;
@@ -116,7 +116,7 @@ public:
 };
 
 //======================Credit=====================
-class Credit : public Payment
+class Credit : public Payment //type 1
 {
 private:
     std::string card_number;
@@ -130,7 +130,7 @@ public:
 };
 
 //======================PayPal=====================
-class PayPal : public Payment
+class PayPal : public Payment //type 2
 {
 private:
     std::string paypal_id;
@@ -143,7 +143,7 @@ public:
 };
 
 //======================WireTransfer=====================
-class WireTransfer : public Payment
+class WireTransfer : public Payment //type 3
 {
 private:
     std::string bank_id;
@@ -182,18 +182,7 @@ void read_customers(std::string theFile)
         // {
         //     std::cout << ele << " ";
         // }
-
-        int theId = std::stoi(chunk.at(0));
-        std::string theName = chunk.at(1);
-        std::string theStreet = chunk.at(2);
-        std::string theCity = chunk.at(3);
-        std::string theState = chunk.at(4);
-        std::string theZip = chunk.at(5);
-        std::string thePhone = chunk.at(6);
-        std::string theEmail = chunk.at(7);
-
-        Customer TheCustomer = Customer(theId, theName, theStreet, theCity, theState, theZip, thePhone, theEmail);
-        customers_vec.push_back(TheCustomer);
+        customers_vec.push_back(chunk);
     }
     ifs.close();
 }
@@ -286,31 +275,70 @@ int main()
     std::ofstream ofs;
     ofs.open("order_report.txt");
 
-    for (const auto &order : orders_vec) // for every order in the orders global vector
+    //for ever customer in global vec
+    //check orders global vec if
+    for (const auto &cust : customers_vec) //for every customer in customers global vector
     {
-        int theId = std::stoi(order[0]);
-        for (const auto &cust : customers_vec) //for every customer in customers global vector
+        std::vector<LineItem> line_items_temp = {};
+        std::vector<Order> all_cust_orders_vec = {};
+        int theCustId = std::stoi(cust[0]);
+        std::string theName = cust.at(1);
+        std::string theStreet = cust.at(2);
+        std::string theCity = cust.at(3);
+        std::string theState = cust.at(4);
+        std::string theZip = cust.at(5);
+        std::string thePhone = cust.at(6);
+        std::string theEmail = cust.at(7);
+        for (const auto &order : orders_vec) // for every order in the orders global vector
         {
-            if (cust.get_cust_id() == theId) //if customer matches the order we are on
+            int theOrderId = std::stoi(order[1]);
+            std::string theDate = order[2];
+
+            if (std::stoi(cust[0]) == theCustId)
             {
-                for (int j = 4; j < order.size(); j++) //loops thru items in orders global vec
+                if (order.size() >= 3) //IF order IS NOT PAYMENT
                 {
-                    auto pair = split(order[j], '-');
-                    int itemId = std::stoi(pair[0]);
-                    for (const auto &itm : items_vec)
+                    for (int j = 3; j < order.size(); j++) //loops thru items in orders global vec
                     {
-                        if (std::stoi(itm[0]) == itemId) //if item in orders vec matches an item in items.txt
+                        auto pair = split(order[j], '-'); //item-quantity pair, split by '-'
+                        int itemId = std::stoi(pair[0]);
+                        int itemQty = std::stoi(pair[1]);
+
+                        for (const auto &itm : items_vec)
                         {
-                            Item theItem = Item(itemId, itm[1], std::stod(itm[2])); //creates new item
-                            LineItem theLineItem = LineItem(itemId, std::stoi(pair[1]));
+                            if (std::stoi(itm[0]) == itemId) //if item in orders vec matches an item in items.txt
+                            {
+                                std::string itemDescription = itm[1];
+                                double itemPrice = std::stod(itm[2]);
+                                Item theItem = Item(itemId, itemDescription, itemPrice); //creates new item
+                                LineItem theLineItem = LineItem(itemId, itemQty);
+                                line_items_temp.push_back(theLineItem);
+                            }
                         }
                     }
+                    Order theOrderObject = Order(theOrderId, theDate, theCustId, line_items_temp);
+                    all_cust_orders_vec.push_back(theOrderObject);
                 }
-                Order theOrder = Order(order[1], order[2], theId, order[3], );
-                cust.cust_orders.push_back(theOrder);
+                else
+                { //IF ORDER IS PAYMENT
+                    int *pptr;
+                    std::string paymentType = order[0];
+                    double amount = order;
+                    if (paymentType == "1")
+                    {
+                        pptr = new Credit()
+                    }
+                    else if (paymentType == "2")
+                    {
+                    }
+                    else if (paymentType == "3")
+                    {
+                    }
+                }
             }
+            //ofs << order.print_detail();
         }
-        //ofs << order.print_detail();
+        Customer TheCustomer = Customer(theCustId, theName, theStreet, theCity, theState, theZip, thePhone, theEmail, all_cust_orders_vec);
     }
     ofs.close();
 
