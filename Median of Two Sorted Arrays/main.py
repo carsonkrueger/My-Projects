d0193e8247d3c0823f629338a5df63dc5c8d663d
carsonkrueger <crkruege@gmail.com>
2021-07-11 13:14:06 -0600

    Program slightly optimized and readme.txt added

--- Median of Two Sorted Arrays/main.py
+++ Median of Two Sorted Arrays/main.py
@@ -1,21 +1,16 @@
 from typing import List
-import math
 
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        nums1 += nums2
-        nums1.sort()
-        nums1Len = len(nums1)
-        median = 0
+        nums3 = nums1 + nums2
+        nums3.sort()
+        nums3Len = len(nums3)
 
-        if (nums1Len % 2 == 1):
-            median = float(nums1[math.floor(nums1Len/2)])
+        if (nums3Len % 2 == 1):
+            return float(nums3[nums3Len//2])
         else:
-            median = (nums1[int(nums1Len/2 - 1)] + nums1[int(nums1Len/2)]) / 2
-
-        print(median)
-        return median
+            return (nums3[int(nums3Len/2 - 1)] + nums3[int(nums3Len/2)]) / 2
 
 
 def main():
@@ -24,7 +19,7 @@ def main():
     list1 = [1, 3]
     list2 = [2, 4]
 
-    sol.findMedianSortedArrays(list1, list2)
+    print(sol.findMedianSortedArrays(list1, list2))
 
 
 if __name__ == "__main__":
