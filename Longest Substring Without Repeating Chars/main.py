b8175e509a9458a7407b42369f44432fa2c7f195
carsonkrueger <crkruege@gmail.com>
2021-07-12 18:13:04 -0600

    Progressing "Longest Substring..." & OOP

---
+++ Longest Substring Without Repeating Chars/main.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        for i in s:
+            for y in
+
+
+def main():
+    s = "abcabcbb"
+    sol = Solution()
+    print(sol.lengthOfLongestSubstring(s))
+
+
+if __name__ == "__main__":
+    main()
