6c0ae19bed718b2a6c03bf906611ef90d4b20a5e
carsonkrueger <crkruege@gmail.com>
2021-07-13 16:35:09 -0600

    working on Longest Substring Program

--- Longest Substring Without Repeating Chars/main.py
+++ Longest Substring Without Repeating Chars/main.py
@@ -1,11 +1,29 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        for i in s:
-            for y in
+        longest = ""
+        if(len(s) == "None"):
+            return 0
+        elif(len(s) == 1):
+            return 1
+
+        for i in range(len(s)):
+            newStr = "" # reset newStr
+            for y in range(len(s)-i):
+                if(s[i+y] in newStr):   # if we find a duplicate
+                    if len(newStr) > len(longest):  # save newStr if its longer
+                        longest = newStr
+                        # print(longest)
+                    break                           # break
+                else:                   # else no duplicate found
+                    newStr += s[i+y]        # add char to newStr
+                    if(i == len(s)):        # if end reached
+                        longest = newStr        # longest is newStr
+
+        return len(longest)
 
 
 def main():
-    s = "abcabcbb"
+    s = "au"
     sol = Solution()
     print(sol.lengthOfLongestSubstring(s))
 
