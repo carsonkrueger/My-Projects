91602b3391dd5ad38346837a1dccbd99aa71eddf
carsonkrueger <crkruege@gmail.com>
2021-09-21 11:36:57 -0600

    sort.py

---
+++ CS 2420/Project 2/sort.py
@@ -0,0 +1,27 @@
+"""Sort.py is a program composed of 4 different sorting algorithms"""
+
+lyst = [7, 6, 10, 5, 9, 2, 1, 15, 7]
+
+
+def quicksort(lyst):
+    """implement quicksort, return the sorted list"""
+
+
+def mergesort(lyst):
+    """implement mergesort, return the sorted list"""
+
+
+def selection_sort(lyst):
+    """implement selection sort, return the sorted list"""
+
+
+def insertion_sort(lyst):
+    """implement insertion sort, return the sorted list"""
+
+
+def main():
+    """main function"""
+
+
+if __name__ == "__main__":
+    main()
