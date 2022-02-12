9d34d2f135a202c4047515e743d4963b0cd74a56
carsonkrueger <crkruege@gmail.com>
2022-02-12 16:21:57 -0700

    removed bootstrap for now

--- snake-game/src/index.js
+++ snake-game/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.css";
+//import "bootstrap/dist/css/bootstrap.css";
 import Board from "./components/Board";
 
 ReactDOM.render(
