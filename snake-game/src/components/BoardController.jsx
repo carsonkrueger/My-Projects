de7209448d41994bead089daca80e6831bb10609
carsonkrueger <crkruege@gmail.com>
2022-06-23 16:40:26 -0600

    Board controller now controls start and reset of the board

--- snake-game/src/components/BoardController.jsx
+++ snake-game/src/components/BoardController.jsx
@@ -1,41 +1,39 @@
-import { Component, React, useEffect } from "react";
+import { Component, React } from "react";
 import Board from "./Board";
 
-class App extends Component {
-  state = {};
+class BoardController extends Component {
+  state = {
+    showBoard: false,
+    resetBoard: false,
+    buttonName: "Start",
+  };
 
-  startBtn = () => {};
+  startBtn = () => {
+    //Reset Board
+    if (this.state.showBoard) {
+      this.setState({ showBoard: false, resetBoard: true });
+    }
+
+    // Initialize Board
+    else {
+      this.setState({ showBoard: true, buttonName: "Reset" });
+      //document.getElementById("strBtn").textContent = "Reset";
+    }
+  };
+
+  render() {
+    return (
+      <>
+        {this.state.showBoard ? (
+          <Board />
+        ) : this.state.resetBoard ? (
+          this.setState({ showBoard: true })
+        ) : null}
+
+        <button onClick={this.startBtn}>{this.state.buttonName}</button>
+      </>
+    );
+  }
 }
 
-// const BoardController = () => {
-//   const [doBoard, setDoBoard] = useEffect(true);
-
-//   const SetBoard = () => {
-//     console.log("method: setBoard");
-
-//     if (doBoard === true) {
-//       return <Board />;
-//     } else {
-//       return <div></div>;
-//     }
-//   };
-
-//   useEffect(() => {}, [doBoard]);
-
-//   const changeDoSetBoardStatus = () => {
-//     setDoBoard = true;
-//     //SetBoard();
-//     //$(".start-button").textContent = "Reset";
-//   };
-
-//   return (
-//     <>
-//       <SetBoard />
-//       <button key="start-button" onClick={changeDoSetBoardStatus}>
-//         Start
-//       </button>
-//     </>
-//   );
-// };
-
-// export default BoardController;
+export default BoardController;
