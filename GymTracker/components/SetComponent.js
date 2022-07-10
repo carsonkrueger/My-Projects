8e190aed9f05902462591e14c16def27910eb1a9
carsonkrueger <crkruege@gmail.com>
2022-07-09 20:59:36 -0600

    styling exercise and set components

--- GymTracker/components/SetComponent.js
+++ GymTracker/components/SetComponent.js
@@ -6,7 +6,7 @@ import {
   StyleSheet,
   TextInput,
 } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign, Feather } from "@expo/vector-icons";
 
 const SetComponent = () => {
   const [numSets, setNumSets] = useState(1);
@@ -19,15 +19,19 @@ const SetComponent = () => {
       </View>
       {/*PREV*/}
       <View style={styles.prevContainer}>
-        <Text></Text>
+        <Text style={styles.prevText}></Text>
       </View>
       {/*WEIGHT*/}
       <View style={styles.weightContainer}>
-        <TextInput></TextInput>
+        <TextInput style={styles.weightText}></TextInput>
       </View>
       {/*REPS*/}
       <View style={styles.repContainer}>
-        <TextInput></TextInput>
+        <TextInput style={styles.repText}></TextInput>
+      </View>
+      {/*CHECK*/}
+      <View style={styles.checkContainer}>
+        <Feather name="check-square" />
       </View>
     </View>
   );
@@ -50,19 +54,30 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
   },
-  weightContainer: {
+  prevText: {
+    color: "#2494f0",
     fontSize: 16,
-    backgroundColor: "#7a7a7a",
-    borderRadius: 5,
+  },
+  weightContainer: {
     flex: 1,
     alignItems: "center",
   },
-  repContainer: {
+  weightText: {
     fontSize: 16,
     backgroundColor: "#7a7a7a",
     borderRadius: 5,
+  },
+  repContainer: {
     flex: 0.8,
     alignItems: "center",
+    fontSize: 16,
+    backgroundColor: "#7a7a7a",
+    borderRadius: 5,
+  },
+  repText: {
+    fontSize: 16,
+    backgroundColor: "#7a7a7a",
+    borderRadius: 5,
   },
   checkContainer: {
     flex: 0.7,
