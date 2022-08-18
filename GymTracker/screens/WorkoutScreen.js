c5683da07dd0c2106f3b551fdc1a18bd71b18d5b
carsonkrueger <crkruege@gmail.com>
2022-08-18 16:02:54 -0600

    version code 11, test ads implemented

--- GymTracker/screens/WorkoutScreen.js
+++ GymTracker/screens/WorkoutScreen.js
@@ -702,7 +702,7 @@ const WorkoutScreen = ({ navigation, route }) => {
       <AdMobBanner
         // style={styles.bottomBanner}
         bannerSize="smartBannerPortrait"
-        adUnitID="ca-app-pub-8357822625939612/5770780706" // Test ID, Replace with your-admob-unit-id
+        adUnitID="ca-app-pub-3940256099942544/6300978111" //"ca-app-pub-8357822625939612/5770780706" // Test ID, Replace with your-admob-unit-id
         servePersonalizedAds={true} // true or false
         // testID={"device"}
         onDidFailToReceiveAdWithError={(e) =>
