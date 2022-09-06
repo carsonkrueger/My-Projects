995e9c89ea1b88e0779667ea980a3f73c60a5a15
carsonkrueger <crkruege@gmail.com>
2022-09-05 23:28:57 -0600

    finishing project

--- CS 3370/Project 1/CircBuf.cpp
+++ CS 3370/Project 1/CircBuf.cpp
@@ -10,8 +10,10 @@ CircBuf::CircBuf() {
 }
 
 CircBuf::CircBuf(size_t reserve) {
-	buffer = new char[reserve] {0};
-	cap = reserve;
+	const int rem = reserve % CHUNK;
+	cap = reserve / CHUNK; 
+	if (rem != 0) cap += CHUNK;
+	buffer = new char[cap] {0};
 }
 
 CircBuf::~CircBuf() {
@@ -20,7 +22,8 @@ CircBuf::~CircBuf() {
 
 void CircBuf::insert(char ch) {
 	if (siz == cap) grow();
-	if (insertIndex >= cap-1) insertIndex = 0;
+	if (insertIndex >= cap) insertIndex = 0;
+
 	buffer[insertIndex] = ch;
 	insertIndex ++;
 	siz ++;
@@ -29,7 +32,8 @@ void CircBuf::insert(char ch) {
 void CircBuf::insert (const char* ch, size_t sz) {
 	for (int i = 0; i < sz; i++) {
 		if (siz == cap) grow();
-		if (insertIndex >= cap-1) insertIndex = 0;
+		if (insertIndex >= cap) insertIndex = 0;
+
 		buffer[insertIndex] = *(ch+i);
 		insertIndex ++;
 		siz ++;
@@ -39,7 +43,8 @@ void CircBuf::insert (const char* ch, size_t sz) {
 void CircBuf::insert(const string& str) {
 	for (int i = 0; i < str.length(); i++) {
 		if (siz == cap) grow();
-		if (insertIndex >= cap-1) insertIndex = 0;
+		if (insertIndex >= cap) insertIndex = 0;
+
 		buffer[insertIndex] = str.at(i);
 		insertIndex ++;
 		siz ++;
@@ -47,9 +52,10 @@ void CircBuf::insert(const string& str) {
 }
 
 char CircBuf::get() {
-	if (getIndex >= cap-1) getIndex = 0;
+	if (getIndex >= cap) getIndex = 0;
 
 	char ch = buffer[getIndex];
+	buffer[getIndex] = 0;
 	getIndex ++;
 	siz --;
 	return ch;
@@ -58,8 +64,13 @@ char CircBuf::get() {
 string CircBuf::get(size_t sz) {
 	string str = "";
 	for (int i = 0; i < sz; i++) {
-		if (getIndex >= cap-1) getIndex = 0;
-		str.push_back(buffer[getIndex]);
+		if (getIndex >= cap) getIndex = 0;
+
+		const char ch = buffer[getIndex];
+		if (ch == 0) break;
+
+		str.push_back(ch);
+		buffer[getIndex] = 0;
 		getIndex ++;
 		siz --;
 	}
@@ -101,10 +112,23 @@ string CircBuf::examine() {
 }
 
 void CircBuf::grow() {
-	char* tempBuf = new char[cap + CHUNK];
-	std::copy(buffer, buffer+cap, tempBuf);
+	char* tempBuf = new char[cap + CHUNK]{0};
+	
+	// if (getIndex > insertIndex) {
+		// const size_t amt = getIndex - cap;
+	const string str = CircBuf::get(siz);
 	delete [] buffer;
 	buffer = tempBuf;
+
+	insertIndex = 0;
+	getIndex = 0;
+	CircBuf::insert(str);
+	
+	// } else {
+	// 	std::copy(buffer, buffer+cap, tempBuf);
+	// 	delete [] buffer;
+	// 	buffer = tempBuf;
+	// }
 	cap += CHUNK;
 }
 
@@ -117,28 +141,29 @@ string CircBuf::convertToString() {
 }
 
 void CircBuf::shrink() {
-	char tempBuf[siz];
-	int count = 0;
+	int prevGetIndex = getIndex;
+	int newCap = (cap - prevGetIndex) / CHUNK;
+	const int rem = (cap - prevGetIndex) % CHUNK;
+	if (rem != 0) newCap += CHUNK;
 
-	for (int i = 0; i < cap; i++) {
-		if (buffer[i] != 0) {
-			tempBuf[count] = buffer[i];
-			count ++;
-		}
-	}
+	char* tempBuf = new char[newCap]{0};
 
+	string str = CircBuf::get(siz);
 	delete [] buffer;
-
 	buffer = tempBuf;
-	cap = siz;
+
+	cap = newCap;
 	insertIndex = 0;
 	getIndex = 0;
+	CircBuf::insert(str);
 }
 
-int main() {
-	CircBuf ab;
-	ab.insert('T');
-	ab.insert("ONE", 3);
-	// ab.get(2);
-	cout << ab.examine() << endl;
-}
+// int main() {
+// 	CircBuf cb;
+// 	cb.insert("aaaaaaaaa");
+// 	cout << cb.examine() << endl;
+// 	cb.get(2);
+// 	cout << cb.examine() << endl;
+// 	cb.shrink();
+// 	cout << cb.examine() << endl;
+// }
