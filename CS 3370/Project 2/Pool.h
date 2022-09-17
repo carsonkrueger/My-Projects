16f66196c64c1be4f7e4c40a274e35a87d12b3da
carsonkrueger <crkruege@gmail.com>
2022-09-16 19:56:09 -0600

    working on project

--- CS 3370/Project 2/Pool.h
+++ CS 3370/Project 2/Pool.h
@@ -1,6 +1,10 @@
 
 
 class Pool {
-public:
-    
+public: 
+   Pool(size_t elemSize, size_t blockSize = 5);
+   ~Pool(); 
+   void* allocate();       // Get a pointer inside a pre-allocated block for a new object 
+   void deallocate(void*); // Free an object's slot (push the address on the "free list")
+   void profile();
 }
