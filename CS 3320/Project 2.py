711ecc2563f5e7efd40007ac9d1b525ad4c0183b
carsonkrueger <crkruege@gmail.com>
2022-09-21 22:14:46 -0600

    most of program is done

--- CS 3320/Project 2.py
+++ CS 3320/Project 2.py
@@ -1,12 +1,48 @@
 import sys
 import math
 
-def ulps(a,b):
-    base = sys.float_info.radix
-    eps = sys.float_info.epsilon
-    prec = sys.float_info.mant_dig
-    inf = math.inf
+base = sys.float_info.radix
+eps = sys.float_info.epsilon
+prec = sys.float_info.mant_dig
+inf = math.inf
+
+def sign(x):
+    if (x < 0):
+        return -1
+    elif (x > 0):
+        return 1
+    elif (x == 0):
+        return 0
+
+def exponent(x):
+    expA = math.frexp(x)[1]
+    if (x == 0): return 0
+    exp = ((prec-2)+expA+1)
+    return expA-1
+
+def fraction(x):
+    print("-TESTING-", end="")
+    return 0
+
+def mantissa(x):
+    print("-TESTING-", end="")
+    return (fraction(x) + 1)
+
+def is_posinfinity(x):
+    if (x == inf): return True
+    return 
     
+def is_neginfinity(x):
+    if (x == -inf): return True
+    return False
+
+def ulp(a):
+    # expA = math.frexp(a)[1]
+    # nextInterval = 2**((prec-2)+expA+1)
+    # return ulps(a,nextInterval)
+    return math.ulp(a)
+
+def ulps(a,b):
     if ((a <= 0 and b >= 0) or (a >= 0 and b <= 0)):
         return inf
     elif (a == inf or b == inf):
@@ -26,43 +62,54 @@ def ulps(a,b):
     expA = math.frexp(ulpA)[1]
     expB = math.frexp(ulpB)[1]
 
-    print(expA, "\n", expB)
-
     numUlps = 0
-    # intervalExp = expA
-    # lowerBound = a
+    intervalExp = expA
+    lowerBound = a
 
-    # while (intervalExp <= expB):
-    #     upperBound = 2**((prec-2)+intervalExp+1)
-    #     intervalUlp = math.ulp(lowerBound)
+    while (intervalExp <= expB):
+        upperBound = 2**((prec-2)+intervalExp+1)
+        intervalUlp = math.ulp(lowerBound)
 
-    #     # if upperbound goes beyond b, set b as the upperbound
-    #     if (upperBound > b):
-    #         upperBound = b
+        # if upperbound goes beyond b, set b as the upperbound
+        if (upperBound > b):
+            upperBound = b
 
-    #     # adds number of ulps in this interval to total ulps
-    #     numUlps += (upperBound - lowerBound)/intervalUlp
-    #     lowerBound = upperBound
-    #     intervalExp += 1
+        # adds number of ulps in this interval to total ulps
+        numUlps += (upperBound - lowerBound)/intervalUlp
+        lowerBound = upperBound
+        intervalExp += 1
 
     return numUlps
 
+def convert(x):
+    def DecimalToBinary(x):
+        if x >= 1:
+            DecimalToBinary(x // 2)
+            x %= 2
+
 if __name__ == "__main__":
-    # ulps(0,20)
-    print(math.frexp(.2)[1],math.frexp(30)[1])
-    # print(ulps(-1.0, -1.0000000000000003)) #1 
-    # print(ulps(1.0, 1.0000000000000003))   #1 
-    # print(ulps(1.0, 1.0000000000000004))   #2 
-    # print(ulps(1.0, 1.0000000000000005))   #2 
-    # print(ulps(1.0, 1.0000000000000006))   #3 
-    # print(ulps(0.9999999999999999, 1.0))   #1 
-    # print(ulps(0.4999999999999995, 2.0)) #9007199254741001 
-    # print(ulps(0.5000000000000005, 2.0)) #9007199254740987 
-    # print(ulps(0.5, 2.0)) #9007199254740992 
-    # print(ulps(1.0, 2.0)) #4503599627370496 
-    # print(2.0**52)        # 4503599627370496.0 
-    # print(ulps(-1.0, 1.0)) #inf 
-    # print(ulps(-1.0, 0.0)) #inf 
-    # print(ulps(0.0, 1.0))  #inf 
-    # print(ulps(5.0, math.inf)) #inf 
-    # print(ulps(15.0, 100.0))  # 12103423998558208
+    y = 6.5 
+    # subMin = np.nextafter(0,1) //subMin = 5e-324 
+    print(sign(y)) #1 
+    print(sign(0.0)) # 0 
+    print(sign(-y)) # -1 
+    print(sign(-0.0)) #0 
+    print(exponent(y)) # 2 
+    print(exponent(16.6)) # 4 
+    print(fraction(0.0)) #0.0 
+    print(mantissa(y)) #1.625 
+    print(mantissa(0.0)) #0.0 
+    # var1 = float(‘nan’) 
+    # print(exponent(var1)) # 1024 
+    print(exponent(0.0)) # 0 
+    # print(exponent(subMin)) # -1022 
+    print(is_posinfinity(math.inf)) # True 
+    print(is_neginfinity(math.inf)) # False 
+    print(not is_posinfinity(-math.inf)) #True 
+    print(is_neginfinity(-math.inf)) #True 
+    print(ulp(y)) # 8.881784197001252e-16 
+    print(ulp(1.0)) # 2.220446049250313e-16 
+    print(ulp(0.0)) # 5e-324 
+    # print(ulp(subMin)) # 5e-324 
+    print(ulp(1.0e15)) # 0.125 
+    print(ulps(1,2)) # 4503599627370496
