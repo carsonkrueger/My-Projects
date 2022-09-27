18a03abf4d287c7c51a08e5ed7fe03593b528491
carsonkrueger <crkruege@gmail.com>
2022-09-27 11:01:42 -0600

    file can read in xml file

--- CS 3370/P3 XML/employee.cpp
+++ CS 3370/P3 XML/employee.cpp
@@ -2,10 +2,17 @@
 #include <fstream>
 using std::cout;
 using std::endl;
+using std::string;
 
 int main(int argc, char* argv[]) {
     for(size_t i=0; i<argc; ++i) {
         cout << argv[i] << endl;
     }
-    fstream in(argv[1])
-}
+
+    std::fstream in(argv[1]);
+
+    string n;
+    while (in >> n) {
+        cout << n << endl;
+    }
+};
