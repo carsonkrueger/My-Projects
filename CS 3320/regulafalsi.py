660487e58395df8bcc29acf6d40160898018ddfe
carsonkrueger <crkruege@gmail.com>
2022-10-11 16:52:41 -0600

    working on regula falsi program

--- CS 3320/regulafalsi.py
+++ CS 3320/regulafalsi.py
@@ -2,13 +2,21 @@ from asyncio.windows_events import NULL
 
 
 def reg(xu, xl, func):
-    root = NULL
+    root = (xu + xl) / 2
     flag = False
     # func val at found root
     numIters = 0
 
     while (True):
-        
+        val = func(root)
+        if (val < 0):
+            xu = root
+        elif (val > 0):
+            xl = root
+        elif (val == 0):
+            print("FOUND ROOT", root)
+        root = (xu + xl) / 2
+            
 
 if __name__ == "__main__":
     pass
