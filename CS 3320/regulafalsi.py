31e592ddc142c6dd274b8b04873f4bda1e9879e4
carsonkrueger <crkruege@gmail.com>
2022-10-11 16:42:37 -0600

    working on regula falsi program

---
+++ CS 3320/regulafalsi.py
@@ -0,0 +1,14 @@
+from asyncio.windows_events import NULL
+
+
+def reg(xu, xl, func):
+    root = NULL
+    flag = False
+    # func val at found root
+    numIters = 0
+
+    while (True):
+        
+
+if __name__ == "__main__":
+    pass
