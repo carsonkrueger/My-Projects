a131d6452b1f6341f6c4b22c719563dbdc71c882
carsonkrueger <crkruege@gmail.com>
2022-10-11 11:46:32 -0600

    renamed folder, working on L4

---
+++ CS 3370/L4/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <array>
+#include <functional>
+
+using std::cout;
+using std::endl;
+using std::array;
+
+void f(int n) {
+    cout << n << "\n";
+}
+
+template<typename T>
+void for_each_if(T s, T e, std::function<void(int)> f, std::function<bool(int)> pred) {
+    while (s != e) {
+        if (pred(*s)) f(*s);
+        s++;
+    }
+}
+
+template<typename T>
+bool is_sorted(T s, T e) {
+    T prev = s;
+    while (s != e) {
+        if (*prev > *s) return false;
+        prev = s;
+        s++;
+    }
+    return true;
+}
+
+template<typename T>
+void range(T s, T e, T v) {
+    while(s != e) {
+        s = v;
+        s++;
+    }
+}
+
+int main() {
+    // for_each_if
+    // array<int,4> a = {1,2,3,4};
+    // for_each_if(a.begin(), a.end(), f, [](int x){return x % 2 == 0;});
+
+    // is_sorted
+    // array<int,4> a = {1,2,3,4};
+    // array<int,4> b = {1,3,2,4};
+    // cout << is_sorted(a.begin(),a.end()) << endl; // 1
+    // cout << is_sorted(b.begin(),b.end()) << endl; // 0
+
+    // range
+    array<int,4> a;
+    int n = range(a.begin(), a.end(), 1);
+    array<int,2> b;
+    range(b.begin(), b.end(), n);
+    copy(a.begin(), a.end(), ostream_iterator<int>(cout, " ")); // 1 2 3 4
+    cout << endl;
+    copy(b.begin(), b.end(), ostream_iterator<int>(cout, " ")); // 5 6
+    cout << endl;
+}
