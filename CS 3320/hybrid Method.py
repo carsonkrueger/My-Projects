672bd46c80b0f4b0bac2728048cdba4e10c1e43e
carsonkrueger <crkruege@gmail.com>
2022-10-22 10:13:49 -0600

    working on p4, and hybrid method

--- CS 3320/hybrid Method.py
+++ CS 3320/hybrid Method.py
@@ -1,78 +1,176 @@
+from re import X
 import sys, math
 
-maxInt = 10000
+minNum = math.ulp(0)
 epsilon = sys.float_info.epsilon
+
+def func1(x):
+    return x * math.cos(x) + math.sin(x)
+
+def func2(x):
+    return math.e**(-x) - x
 
 def falsi(xu, xl, func):
     root = 0
-    flag = 1
-    numIters = 0
 
-    if ((func(xl) * func(xu)) > 0):
-        print("ERROR: same sign:", func(xl), func(xu))
-        flag = -1
-        root = None
-        return root, flag, None, numIters
-    elif (func(xu) == 0 or (func(xu) < epsilon and func(xu) > -epsilon)):
-        print("xu is a root")
-        root = xu
-        flag = 0
-        numIters = 0
-        return root, flag, func(root), numIters
-    elif (func(xl) == 0 or (func(xl) < epsilon and func(xl) > -epsilon)):
-        print("xl is a root")
-        root = xl
-        flag = 0
-        numIters = 0
-        return root, flag, func(root), numIters
+    # if ((func(xl) * func(xu)) > 0):
+    #     print("ERROR: same sign:", func(xl), func(xu))
+    #     root = None
+    #     return root
+    # elif (func(xu) == 0 or (func(xu) < epsilon and func(xu) > -epsilon)):
+    #     print("xu is a root")
+    #     root = xu
+    #     return root
+    # elif (func(xl) == 0 or (func(xl) < epsilon and func(xl) > -epsilon)):
+    #     print("xl is a root")
+    #     root = xl
+    #     return root
 
-    while True:
-        numIters += 1
+    # prevRoot = root
+    if (func(xu) - func(xl) == 0): print(func(xu), func(xl))
+    root = (func(xu)*xl - func(xl)*xu)/(func(xu) - func(xl))
+    # ulp = math.ulp(root)
 
-        prevRoot = root
-        root = (func(xu)*xl - func(xl)*xu)/(func(xu) - func(xl))
-        ulp = math.ulp(root)
+    # if ((func(xl) * func(xu)) < 0):
+    #     xu = root
+    # else:
+    #     xl = root
 
-        if ((func(xl) * func(xu)) < 0):
-            xu = root
-        else:
-            xl = root
+    # func(root) is less than epsilon, 
+    # or 2 consecutive roots are less than an ulp
+    # if (abs(func(root)) <= epsilon):
+    #     print("epsilon")
+    # elif (abs(prevRoot-root) <= ulp):
+    #     print("ulp")
 
-        # func(root) is less than epsilon, 
-        # or 2 consecutive roots are less than an ulp
-        # if (func(root) <= epsilon and func(root) >= -epsilon):
-        #     print("epsilon")
-        #     break
-        if (prevRoot-root <= ulp and prevRoot-root >= -ulp):
-            print("ulp")
-            break
-        elif (numIters >= maxInt):
-            print("Max", maxInt)
-            break
-
-    return root, flag, func(root), numIters
+    return root
 
 def bisect(xu, xl, func):
-    root = 0
+    c = (xu-xl)/2
+    # fofc = func(c)
+
+    # if (fofc < 0):
+    #     xl = c
+    # elif (fofc > 0):
+    #     xu = c
+
+    return c
+
+def secant(prev, cur, func):
+    # if (cur-prev == 0): print("YOOOO")
+    nextCur = cur - (func(cur))/((func(cur)-func(prev))/(cur-prev))
+    return nextCur
+
+def newton(c, func, dfunc):
+    return c - func(c)/dfunc(c)
+
+def swap(c, d):
+    temp = c
+    c = d
+    d = temp
+    return c, d
+
+def checkWithinUlp(x, f, numIters):
+    if (abs(f(x)) <= minNum):
+        print("Num Iterations:", numIters)
+        return True
+    return False
+
+def zero2(a, b, f):
     numIters = 0
 
-    while True:
-        c = (xu-xl)/2
-        fofc = func(c)
-        ulp = math.ulp(c)
+    if (checkWithinUlp(a, f, numIters)): return a
+    if (checkWithinUlp(b, f, numIters)): return b
+
+    while numIters < 10000:
         numIters += 1
+        # first, falsi method
+        # c = falsi(a, b, f)
+        if (f(a) - f(b) == 0): return a
+        c = (f(a)*b - f(b)*a)/(f(a) - f(b))
 
-        if (abs(fofc) <= ulp): # found root
-            return c
-        elif (fofc < 0):
-            xl = c
-        elif (fofc > 0):
-            xu = c
+        # if same sign
+        if (f(a) * f(b) > 0): 
+            # a, c
+            if (c<=a or c>=b):
+                d = bisect(a, b, f)
+                if (checkWithinUlp(d, f, numIters)): return d
+            else:
+                d = secant(a, c, f)
+                if (checkWithinUlp(d, f, numIters)): return d
+        # not same sign
+        else:
+            # b, c
+            d = secant(b, c, f)
+            if (checkWithinUlp(d, f, numIters)): return d
 
-        if (numIters >= maxInt):
-            print("max iterations reached")
-            return c
+        # c is outside interval [a,b]
+        if (d<=a or d>=b):
+            d = bisect(a, b, f)
+            if (checkWithinUlp(d, f, numIters)): return d
+        
+        # swap to make c lower
+        if (c > d): c, d = swap(c,d)
 
+        # check [c,d] length is less than half of [a,b] length
+        if (d-c > (b-a)/2):
+            d = bisect(a, b, f)
+            if (checkWithinUlp(d, f, numIters)): return d
+
+        # # check if within an ulp of zero
+        # if (checkWithinUlp(c, f, numIters)): return c
+        # if (checkWithinUlp(d, f, numIters)): return d
+
+        a = c
+        b = d
+
+def zero(a, b, f):
+    numIters = 0
+    # d = 0
+    
+    if (checkWithinUlp(a, f, numIters)): return a
+    if (checkWithinUlp(b, f, numIters)): return b
+
+    while numIters < 10000:
+        while numIters < 10000:
+            numIters += 1
+
+            c = falsi(-10, 10, f)
+
+            if (checkWithinUlp(c, f, numIters)): return c
+            if (abs(f(c) - f(b)) <= minNum): return c
+
+            if c <= a or c >= b:
+                break
+
+            d = secant(a, c, f)
+
+            if (checkWithinUlp(d, f, numIters)): return d
+            if (abs(f(c) - f(d)) <= minNum): return c
+
+            if (d<=a or d>=b):
+                break
+            if (d-c > (b-a)/2):
+                break
+
+            a = c
+            b = d
+        
+        x = bisect(a, b, f)
+
+        if (checkWithinUlp(x, f, numIters)): return x
+
+        if x < 0:
+            a = x
+        elif x > 0:
+            b = x
+        
+        if (abs(f(a) - f(b)) <= minNum): return a
+
+    print("reached max, interval:", [c,d])
+    print("f(c):", f(c), "f(d):", f(d))
 
 if __name__ == "__main__":
-    pass
+    print(zero(2, 3, func1))
+    print(zero(4, 5, func1))
+    print(zero(0, 1, func2))
--- CS 3370/P4/main.cpp
+++ CS 3370/P4/main.cpp
@@ -5,7 +5,6 @@
 #include <iterator>
 
 int main(int argc, char* argv[]) {
-    
     for(int i = 0; i < argc; ++i){
         std::vector<int> data;
         std::ifstream f(argv[i]);
@@ -14,13 +13,14 @@ int main(int argc, char* argv[]) {
         while(f >> n) data.push_back(-n); // negates
         
         std::vector<int> smoothData;
+        std::vector<int>::iterator it = smoothData.begin();
         const int siz = data.size();
         for (int j = 0; j < siz; ++j) {
             if (j == 0 || j == 1 || j == 3 || j == siz-1 || j == siz-2 || j == siz-3) 
-                smoothData.push_back(data[j]);
+                smoothData.push_back(*it);
             else 
-                smoothData.push_back((data[j-3] + 2*data[j-2] + 3*data[j-1] + 3*data[j] + 3*data[j+1] + 2*data[j+2] + data[j+3]) / 15);
-            
+                smoothData.push_back((it[-3] + 2*it[-2] + 3*it[-1] + 3*it[0] + 3*it[1] + 2*it[2] + it[3]) / 15);
+            i++;
         }
     }
 }
