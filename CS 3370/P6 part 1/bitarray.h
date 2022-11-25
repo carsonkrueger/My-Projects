a547fa25f7100a96e1aea4b347d76f4c6f3b57d6
carsonkrueger <crkruege@gmail.com>
2022-11-25 11:43:25 -0700

    comparator operators, and bracket operators need to be implemented

--- CS 3370/P6 part 1/bitarray.h
+++ CS 3370/P6 part 1/bitarray.h
@@ -48,7 +48,6 @@ public:
     }
     explicit BitArray(const string& s) : siz{s.size()} {
         size_t newCap = (siz/BITS_PER_BLOCK) + 1;
-        std::cout << "init w/ " << newCap << " " << siz << " " << BITS_PER_BLOCK << std::endl;
         cap = newCap;
         bitStr.resize(newCap);
         for (size_t i=0; i<s.size(); ++i) {
@@ -58,6 +57,14 @@ public:
     }
     BitArray(const BitArray& b) = default; // Copy constructor
     BitArray& operator=(const BitArray& b) = default; // Copy assignment
+    BitArray& operator=(const string s){ // Copy String assignment
+        for (size_t i=0; i<s.size(); ++i) {
+            bool val = (s.at(i) == '1') ? true : false;
+            assign_bit(i, val);
+        }
+        siz = s.size();
+        return *this;
+    }
     BitArray(BitArray&& b) noexcept : BITS_PER_BLOCK{b.BITS_PER_BLOCK}, bitStr{b.bitStr}, siz{b.siz}, cap{b.cap} {} // Move constructor
     
     BitArray& operator=(BitArray&& b) noexcept; // Move assignment
@@ -79,9 +86,21 @@ public:
             this->operator+=(val);
         }
     }
-    void erase(size_t pos, size_t nbits = 1); // Remove “nbits” bits at a position
-    void insert(size_t n, bool val); // Insert a bit at a position (slide "right")
-    void insert(size_t pos, const BitArray&); // Insert an entire BitArray object
+    void erase(size_t pos, size_t nbits = 1) { // Remove “nbits” bits at a position
+        std::string s = to_string();
+        s.erase(pos, nbits);
+        this->operator=(s);
+    }
+    void insert(size_t pos, bool val) { // Insert a bit at a position (slide "right")
+        std::string s = to_string();
+        s.insert(pos, val ? "1" : "0");
+        this->operator=(s);
+    } 
+    void insert(size_t pos, const BitArray& b) {  // Insert an entire BitArray object
+        std::string s = to_string();
+        s.insert(pos, b.to_string());
+        this->operator=(s);
+    }
     // Bitwise ops
     // bitproxy operator[](size_t) { // <--------------------------------- put back in
     //     return bitproxy(this);
@@ -95,37 +114,48 @@ public:
     void toggle() { // Toggles all bits
         for (size_t i=0; i<siz; ++i) 
             toggle(i);
-    } 
+    }
     BitArray operator~() const {
         BitArray tmp = *this;
         tmp.toggle();
         return tmp;
     }
     BitArray operator<<(unsigned int n) const { // Shift operators…
-        BitArray<> b{to_string()};
-        for (size_t i=bitStr.size()-1; i>=0; --i){ std::cout << i << " ";
-            b.bitStr[i] >>= n;}
-        return b;
+        std::string s = to_string();
+        s.erase(0, n);
+        for (int i=0; i<n; ++i) s.push_back('0');
+        return BitArray<>{s};
     } 
     BitArray operator>>(unsigned int n) const {
-        BitArray<> b{to_string()};
-        for (size_t i=0; i<bitStr.size(); ++i){ std::cout << i << " ";
-            b.bitStr[i] <<= n;}
-        return b;
+        // for (size_t i=0; i<b.bitStr.size(); ++i){ std::cout << i << " ";
+        //     b.bitStr[i] <<= n;}
+        std::string s = to_string();
+        s.erase(siz-n, n);
+        for (int i=0; i<n; ++i) s.insert(0, "0");
+        return BitArray<>{s};
     }
     BitArray& operator<<=(unsigned int n) {
-        for (size_t i=bitStr.size()-1; i>=0; --i){ std::cout << i << " ";
-            bitStr[i] >>= n;}
+        std::string s = to_string();
+        s.erase(0, n);
+        for (int i=0; i<n; ++i) s.push_back('0');
+        this->operator=(s);
         return *this;
     }
     BitArray& operator>>=(unsigned int n) {
-        for (size_t i=0; i<bitStr.size(); ++i){ std::cout << i << " ";
-            bitStr[i] <<= n;}
+        std::string s = to_string();
+        s.erase(siz-n, n);
+        for (int i=0; i<n; ++i) s.insert(0, "0");
+        this->operator=(s);
         return *this;
     }
 
     // Extraction ops
-    BitArray slice(size_t pos, size_t count) const; // Extracts a new sub-array
+    BitArray slice(size_t pos, size_t count) const { // Extracts a new sub-array
+        std::string s;
+        for (int i=pos; i<pos+count; ++i) 
+            s.push_back(read_bit(i) ? '1' : '0');
+        return BitArray<>{s};
+    }
 
     // Comparison ops
     bool operator==(const BitArray& b) const {
@@ -139,28 +169,35 @@ public:
         return !this->operator==(b);
     }
     bool operator<(const BitArray& b) const {
-        for (size_t i=siz-1; i>0; --i) {
-            if (!read_bit(i) && b.read_bit(i)) return true;
-            else if (!b.read_bit(i) && read_bit(i)) return false;
+        if (size() > b.size()) return true;
+        else if (b.size() > size()) return false;
+        for (size_t i=siz-1; i>=0; --i) {
+            if (bitStr[i] > b.bitStr[i]) return true;
+            else if (b.bitStr[i] > bitStr[i]) return false;
         }
         return false;
+        // for (size_t i=siz-1; i>=0; --i) {
+        //     if (!read_bit(i) && b.read_bit(i)) return true;
+        //     else if (!b.read_bit(i) && read_bit(i)) return false;
+        // }
+        // return false;
     }
     bool operator<=(const BitArray& b) const {
-        for (size_t i=siz-1; i>0; --i) {
+        for (size_t i=siz-1; i>=0; --i) {
             if (!read_bit(i) && b.read_bit(i)) return true;
             else if (!b.read_bit(i) && read_bit(i)) return false;
         }
         return true;
     }
     bool operator>(const BitArray& b) const {
-        for (size_t i=siz-1; i>0; --i) {
+        for (size_t i=siz-1; i>=0; --i) {
             if (read_bit(i) && !b.read_bit(i)) return true;
             else if (b.read_bit(i) && !read_bit(i)) return false;
         }
         return false;
     }
     bool operator>=(const BitArray& b) const {
-        for (size_t i=siz-1; i>0; --i) {
+        for (size_t i=siz-1; i>=0; --i) {
             if (read_bit(i) && !b.read_bit(i)) return true;
             else if (b.read_bit(i) && !read_bit(i)) return false;
         }
