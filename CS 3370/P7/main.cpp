6af85286205c106813ab828783f415709f506422
carsonkrueger <crkruege@gmail.com>
2022-12-08 14:53:12 -0700

    almost done with p7

--- CS 3370/P7/main.cpp
+++ CS 3370/P7/main.cpp
@@ -18,10 +18,11 @@ class Pipeline {
     // static ofstream output;
 public:
     static const size_t nprods = 4, nfilts = 3, ngrps = 10;
+    static size_t n0, n1, n2, n3, n4, n5, n6, n7, n8, n9;
 
     static void produce(int i) {
         srand(time(nullptr)+i*(i+1));
-        for (int i = 0; i < 250; ++i) {
+        for (int i = 0; i < 1000; ++i) {
             int n = rand();     // Get random int
 
             // Get lock for queue; push int
@@ -45,20 +46,23 @@ public:
 
             // Wait for queue to have something to process
             q_cond.wait(qlck,[](){return !q.empty() || !nprod;});
+            if (q.empty()) {
+                break;
+            }
             auto x = q.front();
             q.pop();
-            qlck.unlock();
 
-            unique_lock<mutex> q2lck(q2_sync);
-            if (x % 3 == 0) 
+            // unique_lock<mutex> q2lck(q2_sync);
+            if (x % 3 != 0) 
                 q2.push(x);
-            q2lck.unlock();
+            // q2lck.unlock();
+            qlck.unlock();
 
             // Print trace of consumption
             // lock_guard<mutex> plck(print);
         }
-        --nfilt;
-        q2_cond.notify_all();
+        // --nfilt;
+        // q2_cond.notify_all();
     }
 
     static void group(int i) {
@@ -66,13 +70,49 @@ public:
             // Get lock for sync mutex
             unique_lock<mutex> q2lck(q2_sync);
             // Wait for queue 2 to have something to process
-            q2_cond.wait(q2lck,[](){return !q2.empty() || !nfilt;});
+            q2_cond.wait(q2lck,[i](){return (!q2.empty() || (q2.front() % 10 == i));});
+            // if (q2.empty())
             std::string outf = "out.out";
 
             auto x = q2.front();
             size_t mod = x % 10;
-            if (i != mod) continue;
+            // if (i != mod) continue;
             q2.pop();
+
+            switch(x) {
+                case (0):
+                    ++Pipeline::n0;
+                    break;
+                case (1):
+                    ++Pipeline::n1;
+                    break;
+                case (2):
+                    ++Pipeline::n2;
+                    break;
+                case (3):
+                    ++Pipeline::n3;
+                    break;
+                case (4):
+                    ++Pipeline::n4;
+                    break;
+                case (5):
+                    ++Pipeline::n5;
+                    break;
+                case (6):
+                    ++Pipeline::n6;
+                    break;
+                case (7):
+                    ++Pipeline::n7;
+                    break;
+                case (8):
+                    ++Pipeline::n8;
+                    break;
+                case (9):
+                    ++Pipeline::n9;
+                    break;
+                default:
+                    break;
+            }
 
             outf.insert(3, std::to_string(mod));
             ofstream output {outf, std::ios_base::app};
@@ -80,7 +120,7 @@ public:
             
             // Print trace of consumption
             lock_guard<mutex> plck(print);
-            output << x << " " << (x % 10) << " -> " << i << endl;
+            output << x << endl; //<< " " << (x % 10) << " -> " << i << endl;
         }
     }
 };
@@ -90,6 +130,8 @@ condition_variable Pipeline::q_cond, Pipeline::q2_cond;
 mutex Pipeline::q_sync, Pipeline::q2_sync, Pipeline::print;
 // ofstream Pipeline::output("out0.out");
 atomic_size_t Pipeline::nprod(nprods), Pipeline::nfilt(nfilts);
+size_t Pipeline::n0{0}, Pipeline::n1{0}, Pipeline::n2{0}, Pipeline::n3{0}, Pipeline::n4{0}, 
+    Pipeline::n5{0}, Pipeline::n6{0}, Pipeline::n7{0}, Pipeline::n8{0}, Pipeline::n9{0};
 
 int main() {
     // clear files
@@ -116,4 +158,6 @@ int main() {
         c.join();
     for (auto &g: grps)
         g.join();
+
+    std::cout << "END" << Pipeline::n1 << " " << Pipeline::n2;
 }
