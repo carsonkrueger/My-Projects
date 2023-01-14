a820396f0b9bec755d9cf1bc75012af2f53f89f3
carsonkrueger <crkruege@gmail.com>
2023-01-13 20:03:55 -0700

    moving school projects to private repo

--- CS 3370/Project 2/MyObject.cpp
+++
@@ -1,9 +0,0 @@
-#include "MyObject.h"
-#include <iostream>
-using namespace std;
-
-Pool<MyObject> MyObject::pool{sizeof(MyObject)};
-
-ostream& operator<<(ostream& os, const MyObject& o) {
-    return os << '{' << o.id << ',' << o.name << '}';
-}
